85c7915b837fd217cc7cd522b070617ad2b51ec1
Greg <aaron.r.gregory@gmail.com>
2022-03-20 15:55:38 +0000

    Added storybook and other required dependencies. Tested and working.

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export default extendTheme({
+    colors: {
+        brand: {
+            mustard: '#CBA135',
+            black: '#1b1b1b',
+            orange: '#f46036',
+            blue: '#6290C3',
+            white: '#fdfffc',
+            grey: '#fbfbfb',
+            darkGrey: '#dedede'
+        }
+    }
+});
